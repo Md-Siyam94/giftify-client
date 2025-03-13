459dddbd9f1d5d984d6931ae0b34f8dc4c46fcd1
Abdullah All Kafi <kafi2006jk@gmail.com>
2025-03-14 00:28:37 +0600

    add why choose us section

---
+++ src/layouts/withCommonLayout/pages/(home)/WhyChooseUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhyChooseUs = () => {
+    return (
+        <div>
+           <h4>why choose us</h4> 
+        </div>
+    );
+};
+
+export default WhyChooseUs;
