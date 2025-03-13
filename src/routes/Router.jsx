ddb8096d80f2daf099257f1f9922089d4579309c
Md Siyam <mdsiyam9424@gmail.com>
2025-03-13 23:46:49 +0600

    Navbar is complete

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,7 +3,7 @@ import {
     createBrowserRouter,
 
   } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "../layouts/withCommonLayout/MainLayout";
 import Home from "../pages/(home)/Home";
 
   const router = createBrowserRouter([
