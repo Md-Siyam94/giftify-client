f69ffbab5b1f3f37636ac34e07a9ffaf007b6f18
Hafiz Al Shams <hafizalshams1917@gmail.com>
2025-04-18 19:48:06 +0600

    changed baseURL

--- src/main.jsx
+++ src/main.jsx
@@ -5,32 +5,17 @@ import router from './routes/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './context/AuthContext/AuthProvider.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-<<<<<<< HEAD
-
-
-
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
-=======
 import { ToastContainer } from 'react-toastify'
 
-const queryclient=new QueryClient();
+const queryclient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryclient}>
-      <ToastContainer />
-      <RouterProvider router={router} />
+        <ToastContainer />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProvider>
->>>>>>> b1bbe4fee6ef88cf7db4486cd4ef14804f44f547
   </StrictMode>,
 )
