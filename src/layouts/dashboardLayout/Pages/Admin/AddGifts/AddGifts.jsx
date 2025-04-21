bbe15d9c9d1b3a7c4ffa9e8ae65776ed32eea572
Abdullah All Kafi <kafi2006jk@gmail.com>
2025-04-21 15:45:14 +0600

    added back to home link

--- src/layouts/dashboardLayout/Pages/Admin/AddGifts/AddGifts.jsx
+++ src/layouts/dashboardLayout/Pages/Admin/AddGifts/AddGifts.jsx
@@ -108,9 +108,9 @@ const AddGifts = () => {
                         {...register("category", { required: "Category is required" })}
                     >
                         <option value="">Select Category</option>
-                        <option value="E-gift">Virtual Experiences </option>
-                        <option value="Virtual">Animated Greetings</option>
-                        <option value="Animated">E-Gift Cards</option>
+                        <option value="Virtual Experiences">Virtual Experiences </option>
+                        <option value="Animated Greetings">Animated Greetings</option>
+                        <option value="E-Gift Cards">E-Gift Cards</option>
                     </select>
                     {errors.category && <span className="text-red-600 text-xs">{errors.category.message}</span>}
                 </div>
